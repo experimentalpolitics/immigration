bd6b934646b930d29015f61a3dde0b54748e68c4
Patrick Kraft <kraft.pw@gmail.com>
2019-12-19 10:46:00 +0100

    update gitignore

---
+++ code/mturk.R
@@ -0,0 +1,14 @@
+# =================================================================== #
+# Project: Sources of Misperception
+# Author: Patrick
+# Date: 12/19/2019
+# Summary: check MTurk completion codes etc. before approving HITs
+# =================================================================== #
+
+### Packages
+libray(tidyverse)
+
+
+### Load data
+
+
