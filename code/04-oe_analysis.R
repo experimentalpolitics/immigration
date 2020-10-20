b2b9db3af6f1a39af6adcc89471b7dfbfab43abf
Nicholas R. Davis <nicholas@democracyobserver.org>
2020-10-20 18:17:28 -0500

    Read LIWC percentages back in and join with existing data.

--- code/04-oe_analysis.R
+++ code/04-oe_analysis.R
@@ -13,6 +13,7 @@ library(tidyverse)
 # prep the data for LIWC program
 dat <- read_csv(here("data/immigration_20191219_clean.csv"))
 
+# write out
 dat %>%
     select(., id, taxes_oe) %>%
     write_csv(., "data/taxes_oe.csv")
@@ -20,3 +21,20 @@ dat %>%
 dat %>%
     select(., id, jobs_oe) %>%
     write_csv(., "data/jobs_oe.csv")
+
+# read back in processed scores
+tmp <- read_csv(here("data/LIWC2015 Results (taxes_oe).csv"))
+tmp <- rename(tmp, id = `Source (A)`, tentat_taxes = tentat, certain_taxes = certain)
+
+dat <- tmp %>%
+    select(., id, tentat_taxes, certain_taxes) %>%
+    left_join(dat, ., by = "id")
+
+tmp <- read_csv(here("data/LIWC2015 Results (jobs_oe).csv"))
+tmp <- rename(tmp, id = `Source (A)`, tentat_jobs = tentat, certain_jobs = certain)
+
+dat <- tmp %>%
+    select(., id, tentat_jobs, certain_jobs) %>%
+    left_join(dat, ., by = "id")
+
+rm(tmp)
